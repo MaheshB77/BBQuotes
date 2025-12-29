581dce9ad3298b4237d2bf665664b87ed8dab3be
MaheshB77 <mbansode7696@gmail.com>
2025-12-29 23:37:14 +0530

    Some initial data

--- BBQuotes/ContentView.swift
+++ BBQuotes/ContentView.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Breaking Bad", systemImage: "tortoise") {
+                Text("Breaking Bad Quotes")
+
+            }
+
+            Tab("Better Call Saul", systemImage: "briefcase") {
+                Text("Better Call Saul Quotes")
+
+            }
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
+        
 }
 
 #Preview {
